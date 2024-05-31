55e0b1cea9245661774e2ff38890d31c9cbf8ebf
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-05-31 16:28:17 +0100

    feat: task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+    return [task2, task]
+  } 
+  return [task, task2];
+}

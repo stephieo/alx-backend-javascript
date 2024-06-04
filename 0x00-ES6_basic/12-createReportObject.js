4542c7a3148bb1952c3f34fac95e8a81281a1022
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-04 11:05:15 +0100

    feat: task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,12 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: {...employeesList},
-    getNumberOfDepartments(employeesList){
-      employeesList[]
-
-    }
-
-  }
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      let count = 0;
+      for (const dept in employeesList) {
+        count += 1;
+      }
+      return count;
+    },
+  };
 }

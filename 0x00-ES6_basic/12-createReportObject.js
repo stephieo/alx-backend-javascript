8adb52e1b002d848e7231b5f6b4a0729d135ba11
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-04 11:34:38 +0100

    style: eslint

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,11 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
-      let count = 0;
-      for (const dept in employeesList) {
-        count += 1;
-      }
-      return count;
+      return Object.keys(employeesList).length;
     },
   };
 }

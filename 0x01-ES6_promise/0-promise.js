0f47ec827a7ff0e05349883f6869f92ab5df3c34
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-06 18:25:44 +0100

    style: eslint(task 0)

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-export default function getResponseFromAPI () {
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     const simCondition = true;
     const greet = 'Hello';

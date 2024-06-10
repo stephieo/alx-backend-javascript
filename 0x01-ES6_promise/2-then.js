f26b80990d186606b8b5c30240ca9c824103d076
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-10 02:02:39 +0100

    feat: task 2 ( attach handlers to  promise)

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+function onSuccess(resValue) {
+  if (resValue) {
+    console.log('Got a response from the API');
+  }
+  return {
+    status: 200,
+    body: 'Success',
+  };
+}
+
+function onFailure() {
+  return (Error());
+}
+
+export default function handleResponseFromAPI(promise) {
+  promise.then(onSuccess, onFailure)
+    .then(onSuccess, onFailure)
+    .then(onSuccess, onFailure);
+}

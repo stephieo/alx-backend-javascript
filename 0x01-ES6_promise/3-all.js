951f703b69235064e63711b563d21d1c472afe38
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-11 13:04:50 +0100

    feat: task 3 (resolve multiple promises)

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  let result = '';
+  uploadPhoto()
+    .then((data) => {
+      result = result.concat(data.body);
+      return createUser();
+    }).then((data2) => {
+      result = result.concat(' ', data2.firstName, ' ', data2.lastName);
+      console.log(`${result}`);
+    });
+}

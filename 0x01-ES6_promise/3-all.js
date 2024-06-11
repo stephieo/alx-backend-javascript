9f4d45ad9d32f1c47358c795e3fff6357c208c12
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-11 16:18:36 +0100

    style: eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -12,16 +12,3 @@ export default function handleProfileSignup() {
       console.log('Signup system offline');
     });
 }
-
-// import { uploadPhoto, createUser } from './utils.js';
-
-// export default function handleProfileSignup() {
-//   return Promise.all([
-//     uploadPhoto(),
-//     createUser(),
-//   ]).then((messages) => {
-//     console.log(`${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`);
-//   }).catch(() => {
-//     console.log('Signup system offline');
-//   });
-// }

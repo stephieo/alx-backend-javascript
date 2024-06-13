3fb1ad6c482d1698ac9bf30aa0f4b3004be11bff
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-13 18:12:40 +0100

    feat: function to initialize an array of class instances

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/* eslint-disable */
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const classrooms = [];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  classrooms.push(room1, room2, room3);
+  return classrooms;
+}

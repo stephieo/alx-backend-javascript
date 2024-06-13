daede14524e2b41901bfcabd8462d538bd9154e0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-13 01:46:42 +0100

    feat: task 6 (Promise.allSettled)

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,26 @@
 #!/usr/bin/node
+/* eslint-disable */
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then((settledArray) => {
+    console.log(settledArray);
+    const resultList = [];
+    settledArray.forEach((arrayItem) => {
+      resultList.push({
+        status: arrayItem.status,
+        value: arrayItem.status == 'rejected' ? `Error: ${fileName} cannot be processed` : arrayItem.value,
+      })
+      // resultList.push(arrayItem.status)
+      // if (arrayItem.status == 'rejected') {
+      //   resultList.push(`Error: ${fileName} cannot be processed`)
+      //   } else {
+      //     resultList.push(arrayItem.value);
+      //   }
+        })
+        console.log(resultList);
+  return resultList;
+  });
 }

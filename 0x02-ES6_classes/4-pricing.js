eed31e3b7e145702e6948cc6deceb1429655932b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-15 08:46:01 +0100

    feat: class using  another class as input

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,42 @@
 #!/usr/bin/node
+/* eslint-disable */
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('not a currency');
+    } else if (typeof amount !== 'number') {
+      throw new TypeError('not a number')
+    }
+    this._currency = currency;
+    this._amount = amount;
+  }
+  
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('not a Currency')
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('not a currency');
+    }
+    this._currency = newCurrency;
+  }
+  
+  displayFullPrice() {
+    // const currencyDisplay = ;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`
+  }
+}

04e4978ab7f7a1a7c24c0c38f82afc73bf4d8d23
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-07-24 18:01:15 +0100

    feat: basic command line program using process object

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+// command line program that displays input
+
+process.stdout.write('Welcome to Holberton School, what is your name? \n');
+
+// listener for input in stdin
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is ${data}`);
+  // exit program
+  process.exit();
+});
+
+// process listener for the exit process
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});

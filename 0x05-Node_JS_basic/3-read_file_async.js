1ad40a1a542619f4af82db7d42eafaaa9d7c411c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-01 10:53:54 +0100

    fix: correctly catch error and return promise on success

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,37 @@
 #!/usr/bin/node
-const fs = require('fs/promises');
+  const fs = require('fs/promises');
 
-function countStudents(path) {
-  fs.readFile(path, 'utf-8')
-    .then((contents) => {
-      // seperate entries and remove headers
-      const entries = contents.split('\n');
-      entries.shift();
+  function countStudents(path) {
+    return fs.readFile(path, 'utf-8')
+      .then((contents) => {
+        // seperate entries and remove headers
+        const entries = contents.split('\n');
+        entries.shift();
 
-      // collect length data
-      const numberOfStudents = entries.length;
-      console.log(`Number of students: ${numberOfStudents}`);
+        // collect length data
+        const numberOfStudents = entries.length;
+        console.log(`Number of students: ${numberOfStudents}`);
 
-      // collate and number of students per course
-      let csStudents = 0;
-      const csArray = [];
-      let sweStudents = 0;
-      const sweArray = [];
-      entries.forEach((entry) => {
-        if (entry.includes('CS')) {
-          csStudents += 1;
-          csArray.push(entry.split(',')[0]);
-        } else if (entry.includes('SWE')) {
-          sweStudents += 1;
-          entry.split(',');
-          sweArray.push(entry.split(',')[0]);
-        }
-      });
-      console.log(`Number of students in CS: ${csStudents}. List: ${csArray.join(', ')}`);
-      console.log(`Number of students in SWE: ${sweStudents}. List: ${sweArray.join(', ')}`);
-    })
-    .catch(() => console.log('Cannot load the database'));
-}
+        // collate and number of students per course
+        let csStudents = 0;
+        const csArray = [];
+        let sweStudents = 0;
+        const sweArray = [];
+        entries.forEach((entry) => {
+          if (entry.includes('CS')) {
+            csStudents += 1;
+            csArray.push(entry.split(',')[0]);
+          } else if (entry.includes('SWE')) {
+            sweStudents += 1;
+            entry.split(',');
+            sweArray.push(entry.split(',')[0]);
+          }
+        });
+        console.log(`Number of students in CS: ${csStudents}. List: ${csArray.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweStudents}. List: ${sweArray.join(', ')}`);
 
-module.exports = countStudents;
+      })
+      .catch((err) => {throw new Error('Cannot load the database')});
+  }
+
+  module.exports = countStudents;

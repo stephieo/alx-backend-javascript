b9bcb6043ce60a2244ed4eadabf49450b32f02aa
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-05 17:42:54 +0100

    feat: basic server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
+
+module.exports = app;

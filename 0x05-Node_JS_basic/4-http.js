bc8ecd40e6f24ca79bd89f5ac32b5c2cd1f27f41
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-05 14:32:55 +0100

    feat: simple http server with nodejs

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+server.listen(1245, '127.0.0.1', () => {
+  // console.log("Hello Holberton School");
+});

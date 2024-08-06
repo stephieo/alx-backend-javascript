398b967fa748e72c0199811ad3c526fe8e79c8b5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-06 16:44:29 +0100

    tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const sinon = require('sinon');
+const {describe, it} = require("mocha");
+const assert = require('assert');
+const Utils = require('./utils');
+// const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('check that Utils.calculateNumber is stubbed', function() {
+    const spy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    sendPaymentRequestToApi(100, 20);
+
+    assert(spy.withArgs('The total is: 10').calledOnce);
+    assert(stub.withArgs('SUM', 100, 20).calledOnce);
+
+    spy.restore();
+    stub.restore();
+  });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+class Utils {
+  static  calculateNumber(type, a, b) {
+    let c = 0;
+    const a_r = Math.round(a);
+    const b_r = Math.round(b);
+    
+    if (type === 'SUM') {
+      c = a_r + b_r;
+    } else if (type === 'SUBTRACT') {
+      c = a_r - b_r;
+    } else if (type === 'DIVIDE') {
+        c = b_r === 0 ? 'Error' : a_r / b_r;
+    }
+    
+    return c;
+  }
+}
+
+module.exports = Utils;

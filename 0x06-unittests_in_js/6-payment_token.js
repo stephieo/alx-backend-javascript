a8dd56b4f18e5b9f50782b6ea14149907f5355ce
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-06 19:51:51 +0100

    test: number 6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function getpaymentTokenFromAPI(success) {
+  if (success) {
+    return new Promise ((resolve) => {
+     resolve({data: 'Successful response from the API' });
+    })
+  }
+}
+
+module.exports = getpaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+
+describe("getPaymentTokenFromAPI", function() {
+    it("should return a data property when called with true", function(done) {
+        getPaymentTokenFromAPI(true)
+            .then((data) => {
+                expect(data).to.have.property('data');
+                done();
+            });
+    });
+});

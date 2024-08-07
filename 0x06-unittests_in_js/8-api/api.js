14d5870e57f249c825ff22ebbbdbbfa247c8873e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-07 02:56:58 +0100

    test: integration test 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const express = require('express');
+
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const request = require('request');
+
+describe('test simple express app', function() {
+  // start and end server after all tests
+  // before(function(done) {
+  //   app.listen(7865, done);
+  //   });
+    
+  //   after(function (done) {
+  //     app.close(done)
+  //   });
+    
+    //  write the actual tests within the request callback
+    it('checks the status code  of a request', function (done) {
+      request.get('http://127.0.0.1:7865', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it('checks the  body of a request', function (done) {
+      request.get('http://127.0.0.1:7865', function (error, response, body) {
+        expect(response.body).to.be.equal('Welcome to the payment system');
+        done();
+      });
+    });
+
+});

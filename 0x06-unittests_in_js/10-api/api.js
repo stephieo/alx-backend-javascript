029938c757b63adf165ce354f0add14081e8eed4
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-08-07 04:03:04 +0100

    test: deep equality for objects

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+const express = require('express');
+
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (request, response) => {
+  response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.get('/available_payments', (request, response) => {
+  response.set("Content-Type", "application/json");
+  const payMethods = {
+        payment_methods: {
+        credit_cards: true,
+        paypal: false
+        }
+  }
+  response.send(payMethods);
+});
+
+app.post('/login', (request, response) => {
+  const userName = request.body.userName;
+  if (userName) {
+    response.send(`Welcome ${userName}`);
+  } else {
+    response.status(404).send();
+  }
+});
+
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,64 @@
 #!/usr/bin/node
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const request = require('request');
+
+describe('test simple express app', function() {
+  
+    
+    //  write the actual tests within the request callback
+    it('checks the status code  of a request', function (done) {
+      request.get('http://127.0.0.1:7865', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it('checks the  body of a request', function (done) {
+      request.get('http://127.0.0.1:7865', function (error, response, body) {
+        expect(response.body).to.be.equal('Welcome to the payment system');
+        done();
+      });
+    });
+});
+
+describe('test cart ', function() {
+    //  write the actual tests within the request callback
+    it('checks the status code  of a request', function (done) {
+      request.get('http://127.0.0.1:7865/cart/32', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it('checks the  body of a request', function (done) {
+      request.get('http://127.0.0.1:7865/cart/32', function (error, response, body) {
+        expect(body).to.contain(`Payment methods for cart 32`);
+        done();
+      });
+    });
+});
+    
+describe('available payments', function() {
+    //  write the actual tests within the request callback
+    it('checks the status code  of a request', function (done) {
+      request.get('http://127.0.0.1:7865/available_payments', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it('checks the  body of a request', function (done) {
+      const payMethods = {
+        payment_methods: {
+        credit_cards: true,
+        paypal: false
+        }
+      }
+
+      request.get('http://127.0.0.1:7865/available_payments', {json: true}, function (error, response, body) {
+        expect(body).to.deep.equal(payMethods);
+        done();
+      });
+    });
+});
